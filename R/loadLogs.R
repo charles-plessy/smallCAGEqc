d94de7b84e3cc0426257c36928e75fef0d8d7063
Charles Plessy <plessy@riken.jp>
2013-03-15 17:14:07 +0900

    loadLogs: load all files ending in '.log'.

--- R/loadLogs.R
+++ R/loadLogs.R
@@ -1,26 +1,10 @@
 loadLogs <- function() {
 
+logfiles <- list.files(path='.', pattern='*log')
+
 logs <- data.frame(variable=factor(), sample=factor(), value=numeric())
 
-if ( file.exists('extracted.log') )
-  logs <- rbind(logs, read.table('extracted.log', col.names=colnames(logs)))
-
-if ( file.exists('tagdust.log') )
-  logs <- rbind(logs, read.table('tagdust.log',   col.names=colnames(logs)))
-
-if ( file.exists('spikes.log') )
-  logs <- rbind(logs, read.table('spikes.log',    col.names=colnames(logs)))
-
-if ( file.exists('rdna.log') )
-  logs <- rbind(logs, read.table('rdna.log',      col.names=colnames(logs)))
-
-if ( file.exists('mapped.log') )
-  logs <- rbind(logs, read.table('mapped.log',    col.names=colnames(logs)))
-
-# Maybe the following would be better ?
-# 
-# files <- list.files(..pattern to match files..)
-# results <- lapply(files, read.table, header = TRUE)
+for (logfile in logfiles) {logs <- rbind(logs, read.table(logfile, col.names=colnames(logs)))}
 
 logs <- reshape::cast(logs)
 rownames(logs) <- logs$sample
