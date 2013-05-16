9bcfe88195d1e1d9ded3f90b8a17eab24c6dc860
Charles Plessy <plessy@riken.jp>
2013-05-16 15:47:12 +0900

    Merge logics for automated and manual processings.

--- R/loadLogs.R
+++ R/loadLogs.R
@@ -1,13 +1,48 @@
-loadLogs <- function() {
+setGeneric(
+  "loadLogs",
+  signature="source",
+  function(source)
+    standardGeneric("loadLogs")
+)
 
-logfiles <- list.files(path='.', pattern='*log')
+setMethod(
+  loadLogs,
+  signature=c(source="missing"),
+  function(source)
+    stop("Syntax: loadLogs(source='logs') or loadLogs(source='moirai')")
+)
 
-logs <- data.frame(variable=factor(), sample=factor(), value=numeric())
+setMethod(
+  loadLogs,
+  signature=c(source="character"),
+  function(source) {
+    if (source=="logs") {
+      logfiles <- list.files(path='.', pattern='*log')
+      logs <- data.frame(variable=factor(), sample=factor(), value=numeric())
+      for (logfile in logfiles) {logs <- rbind(logs, read.table(logfile, col.names=colnames(logs)))}
+      logs <- reshape::cast(logs)
+      rownames(logs) <- logs$sample
+      return(logs)
+    }
+    if (source=="moirai") {
+      libs <- read.table(paste('/osc-fs_home/scratch/moirai/nanoCAGE2/input/', LIBRARY, '.multiplex.txt', sep=''), sep='\t', header=T)[,c('samplename', 'group', 'barcode', 'filename')]
+      colnames(libs) <- c('sample', 'group', 'barcode', 'index')
+      rownames(libs) <-libs$sample
 
-for (logfile in logfiles) {logs <- rbind(logs, read.table(logfile, col.names=colnames(logs)))}
+      # Discard empty libraries
+      libs <- libs[colnames(l1),]
+      
+      x <- cast(data=read.table(paste(PROCESSED_DATA, '/text/summary.txt', sep=''), sep='\t'), value='V3', V1 ~ V2)
+      rownames(x) <- sub('NCms1.....','', x$V1)
+      libs$extracted <- x[rownames(libs), 'pairend_extraction']
+      libs$mapped    <- x[rownames(libs), 'genome_mapped']
+      libs$rdna      <- x[rownames(libs), 'removed_by_rRNAdust_ribosomal']
+      libs$tagdust   <- x[rownames(libs), 'removed_by_Tagdust']
+      libs$spikes    <- x[rownames(libs), 'removed_by_rRNAdust_spikes']
+      libs$extracted <- libs$extracted - libs$spikes
+      return(libs)
+    }
+    stop("Syntax: loadLogs(source='logs') or loadLogs(source='moirai')")
+  }
+)
 
-logs <- reshape::cast(logs)
-rownames(logs) <- logs$sample
-
-logs
-}
