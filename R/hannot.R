0803213c1f3337f5fe49134ec9a7a9e36664cef2
Charles Plessy <plessy@riken.jp>
2013-08-15 19:40:03 +0900

    hannot: another function for hierarchical annotation.
    Originally developed in http://osc-internal/trac/fgtt/wiki/AtypicalCapsII

---
+++ R/hannot.R
@@ -0,0 +1,27 @@
+hannot <- function (annot, hierarchy="default") {
+
+# Default hierarchy
+
+if ( hierarchy == "default") {
+  hierarchy <- data.frame(
+    rbind(
+    c("None",            "None"),
+    c("snoscan",         "other"),
+    c("antisense",       "antisense"),
+    c("intron,sense\\|", "intron"),
+    c("exon,sense\\|",   "exon"),
+    c("lncRNA\\|",       "exon"),
+    c("miRNA\\|",        "miRNA"),
+    c("snRNA\\|",        "snRNA"),
+    c("snoRNA\\|",       "snoRNA"),
+    c("tRNA\\|",         "tRNA")),
+    stringsAsFactors=FALSE
+  )
+}
+
+hannot <- rep("other", length(annot))
+for (i in 1:nrow(hierarchy))
+  hannot[grepl(hierarchy[i,1], annot)] <- hierarchy[i,2]
+
+factor(hannot)
+}
