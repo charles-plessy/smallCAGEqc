706b004bf0bf4ca53db50d427e9fe46d16ffbfcb
Charles Plessy <plessy@riken.jp>
2014-05-01 10:49:34 +0900

    Reorganised whitespace.

--- R/loadLogs.R
+++ R/loadLogs.R
@@ -1,47 +1,60 @@
-setGeneric(
-  "loadLogs",
-  signature="source",
-  function(source)
-    standardGeneric("loadLogs")
-)
-
-setMethod(
-  loadLogs,
-  signature=c(source="missing"),
-  function(source)
-    stop("Syntax: loadLogs(source='logs') or loadLogs(source='moirai')")
-)
-
-setMethod(
-  loadLogs,
-  signature=c(source="character"),
-  function(source) {
+.loadLogs <- function(source) {
     if (source=="logs") {
-      logfiles <- list.files(path='.', pattern='*log')
-      logs <- data.frame(variable=factor(), sample=factor(), value=numeric())
-      for (logfile in logfiles) {logs <- rbind(logs, read.table(logfile, col.names=colnames(logs)))}
-      logs <- cast(logs)
-      rownames(logs) <- logs$sample
-      return(logs)
+        logfiles <- list.files(path='.', pattern='*log')
+        logs <- data.frame( variable=factor()
+                          , sample=factor()
+                          , value=numeric())
+        for (logfile in logfiles) {
+            logs <- rbind(logs, read.table(logfile, col.names=colnames(logs)))
+        }
+        logs <- cast(logs)
+        rownames(logs) <- logs$sample
+        return(logs)
     }
     if (source=="moirai") {
-      libs <- read.table(paste('/osc-fs_home/scratch/moirai/nanoCAGE2/input/', LIBRARY, '.multiplex.txt', sep=''), sep='\t', header=T)[,c('samplename', 'group', 'barcode', 'index')]
-      rownames(libs) <-libs$sample
+        libs <- read.table( paste( '/osc-fs_home/scratch/moirai/nanoCAGE2/input/'
+                                 , LIBRARY
+                                 , '.multiplex.txt'
+                                 , sep='')
+                          , sep='\t'
+                          , header=T
+                          )[,c('samplename', 'group', 'barcode', 'index')]
+        rownames(libs) <-libs$sample
 
-      # Discard empty libraries
-      libs <- libs[colnames(l1),]
-      
-      x <- cast(data=read.table(paste(PROCESSED_DATA, '/text/summary.txt', sep=''), sep='\t'), value='V3', V1 ~ V2)
-      rownames(x) <- sub('NCms1.....','', x$V1)
-      libs$extracted <- x[rownames(libs), 'extracted']
-      libs$mapped    <- x[rownames(libs), 'genome_mapped']
-      libs$rdna      <- x[rownames(libs), 'removed_rrna']
-      libs$tagdust   <- x[rownames(libs), 'removed_artefacts']
-      libs$spikes    <- x[rownames(libs), 'removed_spikes']
-      libs$extracted <- libs$extracted - libs$spikes
-      return(libs)
+        # Discard empty libraries
+        libs <- libs[colnames(l1),]
+    
+        x <- cast( data=read.table( paste( PROCESSED_DATA
+                                         , '/text/summary.txt'
+                                         , sep=''
+                                         )
+                                  , sep='\t')
+                                  , value='V3'
+                                  , V1 ~ V2)
+        rownames(x) <- sub('NCms1.....','', x$V1)
+        libs$extracted <- x[rownames(libs), 'extracted']
+        libs$mapped    <- x[rownames(libs), 'genome_mapped']
+        libs$rdna      <- x[rownames(libs), 'removed_rrna']
+        libs$tagdust   <- x[rownames(libs), 'removed_artefacts']
+        libs$spikes    <- x[rownames(libs), 'removed_spikes']
+        libs$extracted <- libs$extracted - libs$spikes
+        return(libs)
     }
     stop("Syntax: loadLogs(source='logs') or loadLogs(source='moirai')")
-  }
+}
+
+setGeneric( "loadLogs"
+          , signature="source"
+          , function(source)
+              standardGeneric("loadLogs")
 )
 
+setMethod( loadLogs
+         , signature=c(source="missing")
+         , function(source)
+             stop("Syntax: loadLogs(source='logs') or loadLogs(source='moirai')")
+)
+
+setMethod( loadLogs
+         , signature=c(source="character")
+         , .loadLogs )
