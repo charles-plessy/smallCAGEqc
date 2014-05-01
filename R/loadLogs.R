272ac25470b6550122ffc85c2900d3387a8c412c
Charles Plessy <plessy@riken.jp>
2014-05-01 09:58:20 +0900

    Import instead of Depend on packages.

--- R/loadLogs.R
+++ R/loadLogs.R
@@ -20,7 +20,7 @@ setMethod(
       logfiles <- list.files(path='.', pattern='*log')
       logs <- data.frame(variable=factor(), sample=factor(), value=numeric())
       for (logfile in logfiles) {logs <- rbind(logs, read.table(logfile, col.names=colnames(logs)))}
-      logs <- reshape::cast(logs)
+      logs <- cast(logs)
       rownames(logs) <- logs$sample
       return(logs)
     }
