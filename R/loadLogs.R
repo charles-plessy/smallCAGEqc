53795d4d9ec5588127689b00aa2ebd33a4557294
Charles Plessy <plessy@riken.jp>
2014-05-01 13:43:21 +0900

    New arguments “multiplex” and “summary” to load local multiplex and summary files (Moirai format).

--- R/loadLogs.R
+++ R/loadLogs.R
@@ -12,15 +12,21 @@
 }
 
 .loadStats <- function(source, multiplex, summary) {
-    libs <- read.table( paste0( '/osc-fs_home/scratch/moirai/nanoCAGE2/input/'
-                              , LIBRARY
-                              , '.multiplex.txt')
+    if (missing(multiplex)) {
+        multiplex <- paste0( '/osc-fs_home/scratch/moirai/nanoCAGE2/input/'
+                           , LIBRARY
+                           , '.multiplex.txt')
+    }
+    if (missing(summary)) {
+        summary <- paste0( PROCESSED_DATA, '/text/summary.txt')
+    }
+    libs <- read.table( multiplex
                       , sep='\t'
                       , header=T
                       )[,c('samplename', 'group', 'barcode', 'index')]
     rownames(libs) <-libs$samplename
 
-    x <- cast( data=read.table( paste0( PROCESSED_DATA, '/text/summary.txt')
+    x <- cast( data=read.table( summary
                               , sep='\t')
                               , value='V3'
                               , V1 ~ V2)
@@ -35,19 +41,19 @@
 }
 
 setGeneric( "loadLogs"
-          , signature="source"
-          , function(source)
+          , signature=c("source", "multiplex", "summary")
+          , function(source, multiplex, summary)
               standardGeneric("loadLogs")
 )
 
 setMethod( loadLogs
-         , signature=c(source="missing")
+         , signature=c(source="missing", multiplex="ANY", summary="ANY")
          , function(source)
              stop("Syntax: loadLogs(source='logs') or loadLogs(source='moirai')")
 )
 
 setMethod( loadLogs
-         , signature=c(source="character")
+         , signature=c(source="character", multiplex="ANY", summary="ANY")
          , function(source) {
                if (source=="logs")   {return (.loadLogs(source))  }
                if (source=="moirai") {return (.loadStats(source)) }
@@ -55,6 +61,9 @@ setMethod( loadLogs
            }
 )
 
-#setMethod( loadLogs
-#         , signature=c(source="character", multiplex="character", summary="character")
-#         , .loadStats )
+setMethod( loadLogs
+         , signature=c(source="character", multiplex="character", summary="character")
+         , function(source, multiplex, summary) {
+               .loadStats(source, multiplex, summary)
+           }
+)
