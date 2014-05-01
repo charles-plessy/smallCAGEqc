8df97f4e49ea96f779cb5a64031ad358917a1c3d
Charles Plessy <plessy@riken.jp>
2014-05-01 13:03:09 +0900

    Split helper function into two: .loadLogs (for local logs) and .loadStats (for Moirai logs).

--- R/loadLogs.R
+++ R/loadLogs.R
@@ -1,39 +1,37 @@
 .loadLogs <- function(source) {
-    if (source=="logs") {
-        logfiles <- list.files(path='.', pattern='*log')
-        logs <- data.frame( variable=factor()
-                          , sample=factor()
-                          , value=numeric())
-        for (logfile in logfiles) {
-            logs <- rbind(logs, read.table(logfile, col.names=colnames(logs)))
-        }
-        logs <- cast(logs)
-        rownames(logs) <- logs$sample
-        return(logs)
+    logfiles <- list.files(path='.', pattern='*log')
+    logs <- data.frame( variable=factor()
+                      , sample=factor()
+                      , value=numeric())
+    for (logfile in logfiles) {
+        logs <- rbind(logs, read.table(logfile, col.names=colnames(logs)))
     }
-    if (source=="moirai") {
-        libs <- read.table( paste0( '/osc-fs_home/scratch/moirai/nanoCAGE2/input/'
-                                  , LIBRARY
-                                  , '.multiplex.txt')
-                          , sep='\t'
-                          , header=T
-                          )[,c('samplename', 'group', 'barcode', 'index')]
-        rownames(libs) <-libs$samplename
+    logs <- cast(logs)
+    rownames(logs) <- logs$sample
+    return(logs)
+}
 
-        x <- cast( data=read.table( paste0( PROCESSED_DATA, '/text/summary.txt')
-                                  , sep='\t')
-                                  , value='V3'
-                                  , V1 ~ V2)
-        rownames(x) <- sub(paste0(LIBRARY, '.'),'', x$V1)
-        libs$extracted <- x[rownames(libs), 'extracted']
-        libs$mapped    <- x[rownames(libs), 'genome_mapped']
-        libs$rdna      <- x[rownames(libs), 'removed_rrna']
-        libs$tagdust   <- x[rownames(libs), 'removed_artefacts']
-        libs$spikes    <- x[rownames(libs), 'removed_spikes']
-        libs$extracted <- libs$extracted - libs$spikes
-        return(libs)
-    }
-    stop("Syntax: loadLogs(source='logs') or loadLogs(source='moirai')")
+.loadStats <- function(source, multiplex, summary) {
+    libs <- read.table( paste0( '/osc-fs_home/scratch/moirai/nanoCAGE2/input/'
+                              , LIBRARY
+                              , '.multiplex.txt')
+                      , sep='\t'
+                      , header=T
+                      )[,c('samplename', 'group', 'barcode', 'index')]
+    rownames(libs) <-libs$samplename
+
+    x <- cast( data=read.table( paste0( PROCESSED_DATA, '/text/summary.txt')
+                              , sep='\t')
+                              , value='V3'
+                              , V1 ~ V2)
+    rownames(x) <- sub(paste0(LIBRARY, '.'),'', x$V1)
+    libs$extracted <- x[rownames(libs), 'extracted']
+    libs$mapped    <- x[rownames(libs), 'genome_mapped']
+    libs$rdna      <- x[rownames(libs), 'removed_rrna']
+    libs$tagdust   <- x[rownames(libs), 'removed_artefacts']
+    libs$spikes    <- x[rownames(libs), 'removed_spikes']
+    libs$extracted <- libs$extracted - libs$spikes
+    return(libs)
 }
 
 setGeneric( "loadLogs"
@@ -50,4 +48,13 @@ setMethod( loadLogs
 
 setMethod( loadLogs
          , signature=c(source="character")
-         , .loadLogs )
+         , function(source) {
+               if (source=="logs")   {return (.loadLogs(source))  }
+               if (source=="moirai") {return (.loadStats(source)) }
+               stop("Syntax: loadLogs(source='logs') or loadLogs(source='moirai')")
+           }
+)
+
+#setMethod( loadLogs
+#         , signature=c(source="character", multiplex="character", summary="character")
+#         , .loadStats )
