b200d6adff32dc4528deea94d0679281c40ec09b
Charles Plessy <plessy@riken.jp>
2015-04-21 11:36:38 +0900

    fldgmPicoGreen: output in long format by default.

--- R/fldgmPicoGreen.R
+++ R/fldgmPicoGreen.R
@@ -1,4 +1,4 @@
-fldgmPicoGreen <- function(FILE, TEMPLATE='PN 100-6260') {
+fldgmPicoGreen <- function(FILE, TEMPLATE='PN 100-6260', FORMAT='long') {
 
   if      (TEMPLATE == 'PN 100-6260')
     linesToSkip = 45
@@ -7,6 +7,9 @@ fldgmPicoGreen <- function(FILE, TEMPLATE='PN 100-6260') {
   else
     stop ('Unknown template number.')
 
+  if (! FORMAT %in% c("long", "wide"))
+    stop ("FORMAT should be 'long' or 'wide'.")
+
   picogreen <- read.xls( FILE
                        , sheet =  3
                        , skip  = linesToSkip
@@ -14,11 +17,23 @@ fldgmPicoGreen <- function(FILE, TEMPLATE='PN 100-6260') {
                        , head  = FALSE
                        , blank.lines.skip = FALSE )[,1:13]
 
+  if ( !all (picogreen[[1]] == c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H')))
+    stop ('Could not find data. Are you indicating the right template ?')
+
   colnames(picogreen) <- c('row', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12')
 
-  tryCatch( data.frame( picogreen
-                       , row.names   = 1
-                       , check.names = FALSE)
-          , error = function (...) stop ('Could not find data. Are you indicating the right template ?')
-  )
+  meltConcentrationTable <- function (TABLE) {
+    picogreen <- melt(TABLE, id.vars='row')
+    colnames(picogreen) <- c('row', 'column', 'concentration')
+    picogreen[,"well"] <- paste(picogreen$row, picogreen$column, sep='')
+    picogreen <- picogreen[, c('well', 'row', 'column', 'concentration')]
+    picogreen
+  }
+
+  if (FORMAT == "long")
+    return(meltConcentrationTable(picogreen))
+  else
+    return(data.frame( picogreen
+                     , row.names   = 1
+                     , check.names = FALSE))
 }
