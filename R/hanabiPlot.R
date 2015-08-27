90457f892a2c208300a8784d8c4c999870eaf4a9
Charles Plessy <plessy@riken.jp>
2015-08-27 10:56:17 +0900

    hanabiPlot: plot feature discovery curves.

---
+++ R/hanabiPlot.R
@@ -0,0 +1,75 @@
+#' hanabiPlot
+#' 
+#' Plot feature discovery curves
+#' 
+#' Plots the number of features (genes, transcripts, ...) detected for a
+#' given number of counts (reads, unique molecules, ...).  Each library is
+#' sub-sampled by rarefaction at various sample sizes, picked to provide
+#' enough points so that the curves look smooth.  The final point is plotted
+#' as an open circle, hence the name "hanabi", which means fireworks in
+#' Japanese. 
+#' 
+#' The rarefactions take time to do, so this step is done by a separate
+#' function, so that the result is easily cached.
+#' 
+#' @param RAR A rarefaction table.
+#' @param S A vector of subsample sizes.
+#' @param log The scale of the plot (by default, log on both axis).
+#' @param type What to plot at the end of the curve (by default,
+#'  open circles).
+#' @param ... Further arguments to be passed to the first plot function,
+#'  that plots the empty frame.
+#' 
+#' @seealso vegan
+
+
+hanabiPlot <- function (RAR, S, log='xy', type='o', ...) {
+  
+  # Accessory function to define the range of the empty frame's axis.
+  minMax <- function (X) c( min(X, na.rm=TRUE)
+                          , max(X, na.rm=TRUE))
+  
+  # Accessory function to make the lines a little transparent.
+  # See https://gist.github.com/mages/5339689#file-add-alpha-r
+  add.alpha <- function(col, alpha=1)
+    apply( sapply(col, col2rgb)/255
+           , 2
+           , function(x)
+             rgb(x[1], x[2], x[3], alpha=alpha))
+  
+  # Accessory function to prepare an empty frame.
+  emptyFrame <- function ()
+    plot( minMax(S)
+          , minMax(RAR)
+          , type='n'
+          , log=log
+          , ...)
+  
+  # Accessory function to plot the lines.
+  rarLines  <- function (X)
+    lines( S
+        , RAR[X,]
+        , col=add.alpha(as.numeric(libs$Error[X]),0.5)
+        , type=type)
+  
+  
+  # Eliminate data points past a cell's sampling size.
+  # Insert NAs everytime the subsampled number of genes
+  # equals the total number of genes (since it means that
+  # subsampling size was larger than real sampling size),
+  # except for the first occurence, which is the last
+  # point of the curve.
+  shiftTrueLeft <- function(TABLE) {
+    n <- ncol(TABLE)
+    TABLE <- cbind(F, TABLE)
+    TABLE[, 1:n]
+  }
+  
+  RAR[shiftTrueLeft(RAR==apply(RAR, 1, max))] <- NA
+  
+  emptFrame()
+  sapply( 1:ncol(RAR), rarLines)
+  points( x = apply(RAR, 1, function(X) max(s[!is.na(X)])) # sampling sizes for each cell
+          , y = apply(RAR, 1, max, na.rm=T) # num. of detected feat. at max. sampl. size
+          , col=as.numeric(libs$Error))
+}
