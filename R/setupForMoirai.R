cd794248a94dd4336004cfcde77dba92734fb082
Charles Plessy <plessy@riken.jp>
2015-08-27 17:13:57 +0900

    Roxygenise.

--- R/setupForMoirai.R
+++ R/setupForMoirai.R
@@ -1,3 +1,37 @@
+#' setupForMoirai
+#'
+#' Exports environment variables to find Moirai files.
+#' 
+#' LOCAL FUNCTION WITH A LOT OF HARDOCODING.  If no timestamp is given,
+#' it will look for the latest processing.
+#' 
+#' Using LIBRARY, MOIRAI_USER and MOIRAI_PROJECT, \code{setupForMoirai} will find
+#' the directory containing the last Moirai run for that library, and export its
+#' path in the environment variable PROCESSED_DATA.
+#' 
+#' It will also set the environment variables GROUP_SHARED, GENE_SYMBOLS and
+#' ANNOTATION, to default values.
+#' 
+#' @param LIBRARY A name identifying a library given in Moirai.
+#' @param MOIRAI_USER A Moirai user name (part of the file path).
+#' @param MOIRAI_PROJECT A Moirai project name (part of the file path).
+#' 
+#' @return 
+#' Prints shell commands that, would have the same effect.  This is useful when
+#' cut-pasting from knitR workflows that use R to change the environment and then
+#' run some shell chunks.
+#' 
+#' Returns PROCESSED_DATA.
+#'
+#' @seealso exportInEnv
+#' 
+#' @examples 
+#' 
+#' ## PROCESSED_DATA <- setupForMoirai(
+#' ##   LIBRARY        = '140609_M00528_0024_000000000-A8CBW',
+#' ##   MOIRAI_USER    = 'nano-fluidigm',
+#' ##   MOIRAI_PROJECT = 'C1_CAGE' )
+
 setupForMoirai <- function( LIBRARY
                           , MOIRAI_USER
                           , MOIRAI_PROJECT
