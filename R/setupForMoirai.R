42644e40ff930e34fee9930ba88dd8b8944ea919
Charles Plessy <plessy@riken.jp>
2015-08-27 17:48:23 +0900

    Provide defaults for hg19 or hg38.
    This function is still broken because exportInEnv looks in the wrong scope.

--- R/setupForMoirai.R
+++ R/setupForMoirai.R
@@ -15,6 +15,10 @@
 #' @param LIBRARY A name identifying a library given in Moirai.
 #' @param MOIRAI_USER A Moirai user name (part of the file path).
 #' @param MOIRAI_PROJECT A Moirai project name (part of the file path).
+#'        Note that some Moirai project names look like user names and
+#'        vice versa.
+#' @param MOIRAI_STAMP A Moirai time stamp.
+#' @param GENOME A genome identifier, to get default annoation and gene definition.
 #' 
 #' @return 
 #' Prints shell commands that, would have the same effect.  This is useful when
@@ -35,13 +39,22 @@
 setupForMoirai <- function( LIBRARY
                           , MOIRAI_USER
                           , MOIRAI_PROJECT
+                          , MOIRAI_STAMP="latest"
+                          , GENOME=c("hg19", "hg38")
                           )
 {
-
   GROUP_SHARED   <- '/osc-fs_home/scratch/gmtu'
-  GENE_SYMBOLS   <- paste(GROUP_SHARED, 'annotation/homo_sapiens/gencode-14/gencode.v14.annotation.genes.bed', sep='/')
-  ANNOTATION     <- paste(GROUP_SHARED, 'annotation/homo_sapiens/100712hg19/100712hg19', sep='/')
-
+  
+  if (identical(GENOME, "hg19")) {
+    GENE_SYMBOLS   <- paste0(GROUP_SHARED, '/annotation/homo_sapiens/gencode-14/gencode.v14.annotation.genes.bed')
+    ANNOTATION     <- paste0(GROUP_SHARED, '/annotation/homo_sapiens/100712hg19/100712hg19', sep='/')
+  } else if (identical(GENOME, "hg38")) {
+    GENE_SYMBOLS   <- paste0(GROUP_SHARED, '/annotation/homo_sapiens/gencode-23/gencode.v23.annotation.genes.bed')
+    ANNOTATION     <- paste0(GROUP_SHARED, '/annotation/homo_sapiens/gencode-23/annot.bed')
+  } else {
+    stop(paste0("Unknown genome. Supported ones: ", paste(GENOME, collapse=' '), "."))
+  }
+  
   latest <- function(RUNS_NAMES) {
     position <- RUNS_NAMES %>%
                   sub('.*\\.','',.) %>%
@@ -53,9 +66,8 @@ setupForMoirai <- function( LIBRARY
   PROCESSED_DATA <- paste ("/osc-fs_home/scratch/moirai" , MOIRAI_USER , "project" , MOIRAI_PROJECT, sep='/') %>%
                     list.files(pattern=LIBRARY, full.names=TRUE) %>%
                     latest
-
-  exportInEnv( "LIBRARY", "MOIRAI_USER", "MOIRAI_PROJECT", "GROUP_SHARED"
-             , "GENE_SYMBOLS", "ANNOTATION", "PROCESSED_DATA")
+  
+  exportInEnv( "LIBRARY", "MOIRAI_USER", "MOIRAI_PROJECT", "GENE_SYMBOLS", "ANNOTATION", "PROCESSED_DATA")
 
   return(PROCESSED_DATA)
 }
