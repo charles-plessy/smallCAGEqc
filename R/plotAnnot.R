d33dc81e2f324b7ce71cbd9221d92f7161d5cb2e
Charles Plessy <plessy@riken.jp>
2015-09-01 21:52:44 +0900

    Roxygen

--- R/plotAnnot.R
+++ R/plotAnnot.R
@@ -1,3 +1,27 @@
+#` plotAnnot
+#'
+#' Plot annotation statistics
+#' 
+#' Using a data frame containing mapping statistics in counts, plot the data as
+#' percentages in stacked barplots.
+#' 
+#' Stacked barplots with error bars inspired from
+#' <http://stackoverflow.com/questions/10417003/stacked-barplot-with-errorbars-using-ggplot2>.
+#' See <http://www.biomedcentral.com/1471-2164/14/665/figure/F1> for example.
+#' 
+#' @param LIBS A data frame with columns named \code{promoter}, \code{exon}, \code{intron}
+#' \code{mapped}, \code{extracted}, \code{rdna}, and \code{tagdust}, that will be
+#' passed to the \code{mapStats} function.
+#' 
+#' @param SCOPE The value on which to normalise (see \code{mapStats}).
+#' 
+#' @param TITLE The title of the plot.
+#' 
+#' @seealso \code{\link{hierarchAnnot}}, \code{\link{loadLogs}}, \code{\link{mapStats}}
+#' 
+#' @examples 
+#' ## plotAnnot(libs, 'annotation', 'Here is the title')
+
 plotAnnot <- function(LIBS, SCOPE, TITLE) {
   ggplot( mapStats(LIBS, scope=SCOPE)
         , aes( x    = group
@@ -15,5 +39,3 @@ plotAnnot <- function(LIBS, SCOPE, TITLE) {
     coord_flip() +
     theme(title=element_text(TITLE))
 }
-
-# Inspiration: http://stackoverflow.com/questions/10417003/stacked-barplot-with-errorbars-using-ggplot2
