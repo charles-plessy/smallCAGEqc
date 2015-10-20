69346c3864a6f422bc7d01289e7d4830defa0cc8
Charles Plessy <plessy@riken.jp>
2015-10-20 10:06:49 +0900

    Show that GGplot2 theme can be set.

--- R/plotAnnot.R
+++ R/plotAnnot.R
@@ -21,6 +21,7 @@
 #' 
 #' @examples 
 #' ## plotAnnot(libs, 'annotation', 'Here is the title')
+#' ## theme_set(theme_bw()) ; plotAnnot(libs, 'annotation', 'Here is the title')
 #' ## plotAnnot(libs, 'annotation', 'Here is the title') + theme_bw()
 
 plotAnnot <- function(LIBS, SCOPE, TITLE, GROUP="default") {
