charles-plessy/smallCAGEqc

commit 8da6a9bc0db070b6527a93cbcd84d7e16076d8a0

Transcript counts and flexible grouping.

R/mapStats.R
@@ -7,10 +7,13 @@
 #' 
 #' See http://stackoverflow.com/questions/10417003/stacked-barplot-with-errorbars-using-ggplot2 about stacked barplot.
 #' 
+#' The \dQuote{mapped} and \dQuote{counts} scopes assume that transcript counts are available.
+#' 
 #' @param libs A data frame with columns named \code{promoter}, \code{exon}, \code{intron}
 #'        \code{mapped}, \code{extracted}, \code{rdna}, and \code{tagdust}.
-#' @param scope The value on which to normalise. \dQuote{all} normalises on the number of extracted tags
-#'        and \dQuote{annotation} normalises on the number of aligned tags.
+#' @param scope The value on which to normalise. \dQuote{all} normalises on the number of extracted tags,
+#'        \dQuote{annotation} on the number of aligned tags, \dQuote{mapped} on the number of
+#'        aligned tags and \dQuote{counts} on the transcript counts.
 #'
 #' @return
 #' Returns mean and standard deviation of normalised mapping statistics, plus absolute
@@ -18,59 +21,62 @@
 #' 
 #' @seealso \code{\link{hierarchAnnot}}, \code{\link{loadLogs}}, \code{\link{plotAnnot}}
 
-mapStats <- function(libs, scope='all') {
-
-if (scope == 'all')
-  if (is.numeric(libs$extracted))
-    scope <- libs$extracted
-  else
-    stop("libs$extracted missing or erroneous.")
-else if (scope == 'annotation')
-  if (is.numeric(libs$mapped))
-    scope <- libs$mapped
-  else
-    stop("libs$mapped missing or erroneous.")
-else
-  stop ('scope must be "all" or "annotation"')
-
-maprates <- with(libs, data.frame(
-  group=group,
-  promoter=(promoter / scope),
-  exon=(exon / scope),
-  intron=(intron / scope),
-  mapped=((mapped  - promoter - intron - exon )/ scope),
-  rdna=(rdna / scope),
-  tagdust=(tagdust / scope)
-))
-
-mapstats <- with(maprates, data.frame(
-  promoter=tapply(promoter, group, mean),
-  exon=tapply(exon, group, mean),
-  intron=tapply(intron, group, mean),
-  mapped=tapply(mapped, group, mean),
-  rdna=tapply(rdna, group, mean),
-  tagdust=tapply(tagdust, group, mean)
-))
-
-mapstats$group <- rownames(mapstats)
-
-mapstats.sd <- with(maprates, data.frame(
-  promoter=tapply(promoter, group, sd),
-  exon=tapply(exon, group, sd),
-  intron=tapply(intron, group, sd),
-  mapped=tapply(mapped, group, sd),
-  rdna=tapply(rdna, group, sd),
-  tagdust=tapply(tagdust, group, sd)
-))
-
-mapstats.sd$group <- rownames(mapstats.sd)
-
-mapstats <- melt(mapstats)
-
-mapstats$sd <- melt(mapstats.sd)$value
-
-mapstats <- ddply(mapstats,.(group),transform,ystart = cumsum(value),yend = cumsum(value) + sd)
-
-return(mapstats)
-
+mapStats <- function(libs, scope=c("all", "annotation", "counts", "mapped"), group="default") {
+    
+  scope <- match.arg(scope)
+  if (identical(group, "default"))
+    group <- libs$group
+  
+  if (scope == 'all') {
+    if (is.numeric(libs$extracted)){
+      total <- libs$extracted
+    } else {
+      stop("libs$extracted missing or erroneous.") }
+  } else if (scope == 'annotation') {
+    if (is.numeric(libs$mapped)) {
+      total <- libs$mapped
+    } else {
+      stop("libs$mapped missing or erroneous.") }
+  } else if (scope == 'counts') {
+    if (is.numeric(libs$counts)) {
+      total <- libs$counts
+    } else {
+      stop("libs$counts missing or erroneous.") }
+  } else if (scope == 'mapped') {
+    if (is.numeric(libs$mapped)) {
+      total <- libs$mapped
+    } else {
+      stop("libs$mapped missing or erroneous.") }
+  }
+  
+  if (! ("tagdust" %in% colnames(libs)))
+    libs$tagdust <- 0
+  
+  if (scope == "counts") {
+    libs$intergenic = with(libs, counts - promoter - intron - exon)
+    columns <- c("promoter","exon","intron","intergenic")
+  } else if (scope == "mapped") {
+    libs$intergenic = with(libs, counts - promoter - intron - exon)
+    libs$duplicates = with(libs, mapped - counts)
+    columns <- c("promoter","exon","intron","intergenic", "duplicates")
+  } else {
+    libs$mapped <- with(libs, mapped  - promoter - intron - exon)
+    columns <- c("promoter","exon","intron","mapped","rdna", "tagdust")
+  }
+  
+  doMean <- function (X) tapply(libs[,X] / total, group, mean)
+  doSd   <- function (X) tapply(libs[,X] / total, group, sd  )
+  
+  mapstats          <- sapply(columns, doMean) %>% data.frame
+  mapstats$group    <- rownames(mapstats)
+  
+  mapstats.sd       <- sapply(columns, doSd)   %>% data.frame
+  mapstats.sd$group <- rownames(mapstats.sd)
+  
+  mapstats          <- melt(mapstats)
+  mapstats$sd       <- melt(mapstats.sd)$value
+  
+  mapstats          <- ddply(mapstats,.(group),transform,ystart = cumsum(value),yend = cumsum(value) + sd)
+  
+  mapstats
 }

R/plotAnnot.R
@@ -22,8 +22,8 @@
 #' @examples 
 #' ## plotAnnot(libs, 'annotation', 'Here is the title')
 
-plotAnnot <- function(LIBS, SCOPE, TITLE) {
-  ggplot( mapStats(LIBS, scope=SCOPE)
+plotAnnot <- function(LIBS, SCOPE, TITLE, GROUP="default") {
+  ggplot( mapStats(LIBS, scope=SCOPE, group=GROUP)
         , aes( x    = group
              , y    = value
              , fill = variable)

man/mapStats.Rd
@@ -4,14 +4,16 @@
 \alias{mapStats}
 \title{mapStats}
 \usage{
-mapStats(libs, scope = "all")
+mapStats(libs, scope = c("all", "annotation", "counts", "mapped"),
+  group = "default")
 }
 \arguments{
 \item{libs}{A data frame with columns named \code{promoter}, \code{exon}, \code{intron}
 \code{mapped}, \code{extracted}, \code{rdna}, and \code{tagdust}.}
 
-\item{scope}{The value on which to normalise. \dQuote{all} normalises on the number of extracted tags
-       and \dQuote{annotation} normalises on the number of aligned tags.}
+\item{scope}{The value on which to normalise. \dQuote{all} normalises on the number of extracted tags,
+       \dQuote{annotation} on the number of aligned tags, \dQuote{mapped} on the number of
+       aligned tags and \dQuote{counts} on the transcript counts.}
 }
 \value{
 Returns mean and standard deviation of normalised mapping statistics, plus absolute
@@ -25,6 +27,8 @@ Using a data frame containing mapping statistics in counts, transform the data i
 percentages that can be used for stacked barplots.
 
 See http://stackoverflow.com/questions/10417003/stacked-barplot-with-errorbars-using-ggplot2 about stacked barplot.
+
+The \dQuote{mapped} and \dQuote{counts} scopes assume that transcript counts are available.
 }
 \seealso{
 \code{\link{hierarchAnnot}}, \code{\link{loadLogs}}, \code{\link{plotAnnot}}

man/plotAnnot.Rd
@@ -4,7 +4,7 @@
 \alias{plotAnnot}
 \title{Plot annotation statistics}
 \usage{
-plotAnnot(LIBS, SCOPE, TITLE)
+plotAnnot(LIBS, SCOPE, TITLE, GROUP = "default")
 }
 \arguments{
 \item{LIBS}{A data frame with columns named \code{promoter}, \code{exon}, \code{intron}

commit 5a6c42c4f3fa30d595ecafc140703e35b9bae181

Support "group" factor with a single level.
Fixes a bug introduced in commit 8da6a9bc0db070b6527a93cbcd84d7e16076d8a0

R/mapStats.R
@@ -83,10 +83,12 @@ mapStats <- function(libs, scope=c("all", "annotation", "counts", "mapped", "qc"
   doMean <- function (X) tapply(libs[,X] / total, group, mean)
   doSd   <- function (X) tapply(libs[,X] / total, group, sd  )
   
-  mapstats          <- sapply(columns, doMean) %>% data.frame
+  # "simplify" needs to be FALSE so that conversion to data frame works even
+  # when the group contains only a single factor.
+  mapstats          <- sapply(columns, doMean, simplify = FALSE) %>% data.frame
   mapstats$group    <- rownames(mapstats)
   
-  mapstats.sd       <- sapply(columns, doSd)   %>% data.frame
+  mapstats.sd       <- sapply(columns, doSd, simplify = FALSE)   %>% data.frame
   mapstats.sd$group <- rownames(mapstats.sd)
   
   mapstats          <- reshape::melt(mapstats)