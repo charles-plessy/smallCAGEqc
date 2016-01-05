8a08b64e4af93a916336c08c661b28aefa25a8cf
Charles Plessy <plessy@riken.jp>
2016-01-05 10:56:06 +0900

    Return zero when no genes are expressed.
    Otherwise, the function crashes when a column is all zeros.

--- R/countSymbols.R
+++ R/countSymbols.R
@@ -1,8 +1,11 @@
 countSymbols <- function(g2) {
-  countSymbolsVector <- function(x)
+  countSymbolsVector <- function(x) {
+    if (all (x == 0))
+      return (0)
     rownames(g2)[x >0] %>%
     listSymbols %>%
     length
+  }
 
   lapply(g2, countSymbolsVector) %>%
   unlist
