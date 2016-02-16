0cc96d69b378686944f841582a9c4eb5aab00e6e
Charles Plessy <plessy@riken.jp>
2016-02-16 10:24:13 +0900

    Accept only data frames and warn about NaNs.

--- R/TPM.R
+++ R/TPM.R
@@ -1,4 +1,10 @@
 TPM <- function(clusters){
+  
+  if (! is.data.frame(clusters))
+    stop("Input must be a data frame.")
+  
+  if (any(colSums(clusters) == 0))
+    warning("Some samples have no counts; NaN values will be returned.")
 
 clusters.tpm <- data.frame(prop.table(as.matrix(clusters),2) * 1000000)
 
