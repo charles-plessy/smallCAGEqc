803a2b211d0743f8ec315af312db8dcbe956485c
Charles Plessy <plessy@riken.jp>
2016-04-28 14:47:15 +0900

    Added a margin option to TPM-normalise on rows.

--- R/TPM.R
+++ R/TPM.R
@@ -8,19 +8,26 @@
 #' 
 #' @param clusters A data frame of tag counts, where each row is a cluster and each
 #'        column is a library.
+#' @param margin 1 for rows and 2 for columns (default)
 #'
 #' @return Returns a data frame expression values normalised in tags per million,
 #' where each row is a cluster and each column is a library.
+#'
+#' TPM uses prop.table() internally.
 #' 
+#' @seealso prop.table
+#'  
 #' @examples
 #' TPM(data.frame(A=c(1,0,3), b=c(2,5,9)))
+#' TPM(data.frame(A=c(1,0,3), b=c(2,5,9)), margin = 1)
 #' 
 #' # Should warn about NaN
 #' # TPM(data.frame(A=c(1,0,3), b=c(2,5,9), c=c(0,0,0)))
 #' 
+#' @importFrom magrittr "%>%" multiply
 #' @export TPM
 
-TPM <- function(clusters){
+TPM <- function(clusters, margin = 2){
   
   if (! is.data.frame(clusters))
     stop("Input must be a data frame.")
@@ -28,7 +35,11 @@ TPM <- function(clusters){
   if (any(colSums(clusters) == 0))
     warning("Some samples have no counts; NaN values will be returned.")
 
-clusters.tpm <- data.frame(prop.table(as.matrix(clusters),2) * 1000000)
+clusters.tpm <- clusters %>%
+  as.matrix %>%
+  prop.table(margin) %>%
+  multiply_by(1e6) %>%
+  data.frame
 
 colnames(clusters.tpm) <- colnames(clusters)
 
