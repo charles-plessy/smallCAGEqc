0a59711eabf72419db082c2219549bcfa13530b0
Charles Plessy <plessy@riken.jp>
2016-05-11 14:27:29 +0900

    Roxygen.

--- R/hannot.R
+++ R/hannot.R
@@ -1,3 +1,44 @@
+#' hannot
+#' 
+#' Hierarchical annotation of the CAGE clusters
+#' 
+#' Parses a vector of annotations in character format an returns a vector of
+#' annotations as factors, where one single keyword has been selected from a
+#' provided list or the default list (see below).
+#' 
+#' @param annot A character vector of annotations.
+#' @param hierarchy Either the character \dQuote{default}, or a data frame
+#'        whith patterns in its first column and keywords in its second
+#'        column.  The default data frame is picked when \dQuote{default} is
+#'        passed as argument.
+#' 
+#' The default data frame is as follows (as of version 0.2.1).
+#' 
+#' \preformatted{
+#' None               None
+#' snoscan            other
+#' antisense          antisense
+#' intron,sense\\|    intron
+#' exon,sense\\|      exon
+#' lncRNA\\|          exon
+#' miRNA\\|           miRNA
+#' snRNA\\|           snRNA
+#' snoRNA\\|          snoRNA
+#' tRNA\\|            tRNA
+#' }
+#' 
+#' @return A vector of annotations as factors, where the levels are sorted in
+#' the hierarchical order.  For instance, with the data frame above, if an
+#' annotation matches for the keywords \sQuote{miRNA} and \sQuote{snoRNA},
+#' then the reported one will be \sQuote{snoRNA}.
+#' 
+#' @examples
+#' 
+#' x <- c('exon,sense|foo', 'foo', 'antisense of bar', 'intron,sense|bar'
+#'       , 'antisense of baz;snoRNA|SNORA70')
+#' hannot(x)
+#' cbind(x, as.character(hannot(x)))
+
 hannot <- function (annot, hierarchy="default") {
 
 # Default hierarchy
