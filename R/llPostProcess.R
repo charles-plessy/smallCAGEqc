867b12fb06820e99d49f3555bfd45e140fce7977
Charles Plessy <plessy@riken.jp>
2016-05-18 10:25:50 +0900

    New "C1 CAGE" type for llPostProcess.

--- R/llPostProcess.R
+++ R/llPostProcess.R
@@ -9,24 +9,59 @@
 #' 
 #' @details 
 #' 
-#' If the TYPE is "nano-fluidigm", the generic "RunA" and "RunB" names are
-#' replaced with the C1 run IDs found in the "RunA" and "RunB" variables,
-#' which must exist.  These new sample names are then used are row names.
-#' They are parsed to produce new columns for "Well", "row" and "column"
-#' IDs in the 96-well plate format.
+#' If the TYPE is \sQuote{C1 CAGE}, the sample names are assumed to be made
+#' of a run name, followed by an underscore, followed by a coordinate in
+#' 96-well plate format.  These sample names are parsed to parsed to produce
+#' new columns for \sQuote{Run}, \sQuote{Well}, \sQuote{row} and
+#' \sQuote{column} accordingly.
+#' 
+#' If the TYPE is \sQuote{nano-fluidigm}, the generic \sQuote{RunA} and
+#' \sQuote {RunB} names are replaced with the C1 run IDs found in the
+#' \code{RunA} and \code{RunB} variables, which must exist.  These new sample
+#' names are then parsed like in the \sQuote{C1 CAGE} type.
+#' 
+#' @family smallCAGEqc metadata functions
+#' 
+#' @examples
+#' libs <-
+#' loadMoiraiStats(
+#'     summary   = system.file("extdata/summary.txt", package="smallCAGEqc")
+#'   , multiplex = system.file("extdata/samplename_to_sampleid.txt", package="smallCAGEqc")
+#'   , pipeline  = "OP-WORKFLOW-CAGEscan-short-reads-v2.0" )
+#'   
+#'  libs %>% head
+#'  libs %>% llPostProcess("nano-fluidigm") %>% head
+#' 
+#' @importFrom magrittr '%<>%'
+#' @export llPostProcess
 
 llPostProcess <- function (TABLE, TYPE='') {
+  
+  samplesnamesToWell <- function(TABLE) {
+    TABLE$Well   <- sub('.*_', '', TABLE$samplename)
+    TABLE$row    <- sub('.',   '', TABLE$Well)
+    TABLE$column <- sub('..$', '', TABLE$Well)
+    TABLE
+  }
+  
+  samplenamesToRun <- function(TABLE) {
+    TABLE$Run <- sub("_.*", "", TABLE$samplename)
+    TABLE$Run <- factor(TABLE$Run)
+    TABLE
+  }
+  
   if (TYPE == "nano-fluidigm") {
     if (!exists("RunA") | !exists("RunB"))
       stop ('"RunA" and "RunB" must be defined.')
     TABLE$samplename <- sub('RunA', RunA, TABLE$samplename)
     TABLE$samplename <- sub('RunB', RunB, TABLE$samplename)
     rownames(TABLE) <- TABLE$samplename
-    TABLE$Run <- sub("_.*", "", TABLE$samplename)
-    TABLE$Run <- factor(TABLE$Run)
-    TABLE$Well   <- sub('.*_', '', TABLE$samplename)
-    TABLE$row    <- sub('.',   '', TABLE$Well)
-    TABLE$column <- sub('..$', '', TABLE$Well)
+    TABLE %<>% samplenamesToRun %>% samplesnamesToWell
   }
+  
+  if (TYPE == "C1 CAGE") {
+    TABLE %<>% samplenamesToRun %>% samplesnamesToWell
+  }
+  
   TABLE
 }
