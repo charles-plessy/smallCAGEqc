5bc42cba236ec86425d0729ca2397bb96c1fc671
Charles Plessy <plessy@riken.jp>
2016-05-22 16:34:37 +0900

    Roxygen.

---
+++ R/smallCAGEqc.R
@@ -0,0 +1,12 @@
+#' smallCAGEqc
+#' 
+#' A package to quickly generate reports about CAGE libraries.
+#' 
+#' Ad-hoc functions to QC shallow-sequenced libraries, for instance from MiSeq.
+#' 
+#' Under construction
+#' 
+#' @docType package
+#' @name smallCAGEqc
+
+NULL
