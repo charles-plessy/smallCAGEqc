6b5fefd2b40a48ce5a2e5eea8425667ecfd7c735
Charles Plessy <plessy@riken.jp>
2016-08-31 11:34:49 +0900

    Correct typo in documentation.

--- R/loadMoiraiStats.R
+++ R/loadMoiraiStats.R
@@ -36,7 +36,7 @@
 #'  Alternatively, returns ERCC spike counts when \sQuote{ercc} is set to \sQuote{TRUE}.
 #'  
 #' @family smallCAGEqc metadata functions
-#' @seealso \code{\link{hierarchAnnot}}, code{\link{loadLogs}}, \code{\link{mapStats}}
+#' @seealso \code{\link{hierarchAnnot}}, \code{\link{loadLogs}}, \code{\link{mapStats}}
 #' 
 #' @examples 
 #' libs <- loadMoiraiStats(
