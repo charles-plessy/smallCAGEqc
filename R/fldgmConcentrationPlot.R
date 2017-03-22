268204c81bb72cc2fd1cc62bef6135d346ec086f
Charles Plessy <plessy@riken.jp>
2017-03-22 14:31:59 +0900

    fldgmConcentrationPlot: coerce input to data frame.
    This allows to run the function on DataFrames.

--- R/fldgmConcentrationPlot.R
+++ R/fldgmConcentrationPlot.R
@@ -2,12 +2,29 @@
 #' 
 #' Plot histogram of cDNA concentrations after a C1 run
 #' 
-#' @param LIBS A \sQuote{libs} table, that contains at least a
-#'        \sQuote{Concentration} and a \sQuote{Run} column
+#' @param LIBS A metadata table, often called \sQuote{libs} table,
+#'        that contains at least a \sQuote{Concentration} and a \sQuote{Run}
+#'        column.  The table will be coerced as a data frame
+#'        (without checking names), so that Bioconductor's DataFrame
+#'        objects can also be used.#'        
 #' @param scales The \sQuote{scales} argument for the plot.
 #'        Default: \sQuote{fixed}.
 #' @param group A formula to be passed to \sQuote{ggplot2}'s
 #'        \sQuote{facet_wrap} function.
+#'        
+#' @examples 
+#' # Example data
+#' libs <- read.table(system.file("extdata/libs-with-all-metadata.tsv", package="smallCAGEqc"))
+#' 
+#' # Plot a single histogram
+#' fldgmConcentrationPlot(libs)
+#' 
+#' # Faced by group
+#' fldgmConcentrationPlot(libs, group = ~group)
+#' 
+#' # Test coercion to data frame.
+#' if(require(S4Vectors))
+#'   fldgmConcentrationPlot(DataFrame(libs))
 #' 
 #' @import ggplot2
 #' @export fldgmConcentrationPlot
@@ -15,6 +32,7 @@
 fldgmConcentrationPlot <- function ( LIBS
                                    , scales="fixed"
                                    , group=~Run) {
+  LIBS <- data.frame(LIBS, check.names = FALSE)
   qplot( data   = LIBS
        , Concentration
        , geom   = "histogram"
