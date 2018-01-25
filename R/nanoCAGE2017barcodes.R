98efc338e7b64ede6c3acdf04dc450a0cf8127b4
Charles Plessy <plessy@riken.jp>
2018-01-25 13:24:45 +0900

    New lazy-loaded "nanoCAGE2017barcodes" dataset.
    Provides the sequence of the 96 barcodes published in Poulain et al., 2017.

---
+++ R/nanoCAGE2017barcodes.R
@@ -0,0 +1,20 @@
+#' 96 hexameric barcode sequences for sample indexing
+#'
+#' Sequence of the 96 barcodes used in Poulain \emph{et al.}, 2017. NanoCAGE
+#' libraries can index samples by inserting barcodes in the template-switching
+#' oligonucleotide.  A list of 96 barcodes, was published in Poulain \emph{et
+#' al.}, 2017.  The barcodes were selected to avoid palindromes, polynucleotide
+#' tracts, and sequence similarity.
+#' 
+#' @format A data frame with one column named \code{barcodes}.
+#'
+#' @author Jean-Étienne Morlighem, Charles Plessy
+#'
+#' @references Poulain S, Kato S, Arnaud O, Morlighem JÉ, Suzuki M, Plessy C,
+#'   Harbers M. NanoCAGE: A Method for the Analysis of Coding and Noncoding
+#'   5'-Capped Transcriptomes. Methods Mol Biol. 2017 1543:57-109
+#'
+#' @examples
+#' nanoCAGE2017barcodes
+
+"nanoCAGE2017barcodes"
